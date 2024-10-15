73d7ac5ac9fcf41d07e00f14dd423385c5d8cc37
Vitor Jesus <vitorjdev@gmail.com>
2024-10-15 12:15:01 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,203 @@
+interface Operation {
+    value: number;
+}
+
+class Value implements Operation {
+    constructor(public readonly value: number) { }
+}
+
+class Sum implements Operation {
+    constructor(
+        private readonly leftOperation: Operation,
+        private readonly rightOperation: Operation
+    ) { }
+
+    get value() {
+        return this.leftOperation.value + this.rightOperation.value;
+    }    
+}
+
+class Subtraction implements Operation {
+    constructor(
+        private readonly leftOperation: Operation,
+        private readonly rightOperation: Operation
+    ) { }
+
+    get value() {
+        return this.leftOperation.value - this.rightOperation.value;
+    }
+}
+
+class Multiplication implements Operation {
+    constructor(
+        private readonly leftOperation: Operation,
+        private readonly rightOperation: Operation
+    ) { }
+
+    get value() {
+        return this.leftOperation.value * this.rightOperation.value;
+    }
+}
+
+class Divide implements Operation {
+    constructor(
+        private readonly leftOperation: Operation,
+        private readonly rightOperation: Operation
+    ) { }
+
+    get value() {
+        return this.leftOperation.value / this.rightOperation.value;
+    }
+}
+
+/*
+    Will apply this formula: ((10 + 20) * (5 - 3)) / ((6 + 2) * (4 / 2))
+*/
+const example1 = new Divide(
+    new Multiplication(
+        new Sum(
+            new Value(10), 
+            new Value(20)
+        ),
+        new Subtraction(
+            new Value(5), 
+            new Value(3)
+        )
+    ),
+    new Multiplication(
+        new Sum(
+            new Value(6), 
+            new Value(2)
+        ),
+        new Divide(
+            new Value(4), 
+            new Value(2)
+        )
+    )
+);
+
+/*
+    Will apply this formula: (50 - ((10 * 3) + (8 / 2))) + (20 * (4 + 1))
+*/
+const example2 = new Sum(
+    new Subtraction(
+        new Value(50),
+        new Sum(
+            new Multiplication(
+                new Value(10), 
+                new Value(3)
+            ),
+            new Divide(
+                new Value(8), 
+                new Value(2)
+            )
+        )
+    ),
+    new Multiplication(
+        new Value(20),
+        new Sum(
+            new Value(4), 
+            new Value(1)
+        )
+    )
+);
+
+/*
+    Will apply this formula: ((15 + 5) - (20 / (4 + 1))) * ((10 * 2) + (30 / 6))
+*/
+const example3 = new Multiplication(
+    new Subtraction(
+        new Sum(
+            new Value(15), 
+            new Value(5)
+        ),
+        new Divide(
+            new Value(20), 
+            new Sum(
+                new Value(4), 
+                new Value(1)
+            )
+        )
+    ),
+    new Sum(
+        new Multiplication(
+            new Value(10), 
+            new Value(2)
+        ),
+        new Divide(
+            new Value(30), 
+            new Value(6)
+        )
+    )
+);
+
+/*
+    Will apply this formula: (((30 / 3) + (5 * 2)) - (12 / (4 - 1))) * (25 - (10 + 5))
+*/
+const example4 = new Multiplication(
+    new Subtraction(
+        new Sum(
+            new Divide(
+                new Value(30), 
+                new Value(3)
+            ),
+            new Multiplication(
+                new Value(5), 
+                new Value(2)
+            )
+        ),
+        new Divide(
+            new Value(12), 
+            new Subtraction(
+                new Value(4), 
+                new Value(1)
+            )
+        )
+    ),
+    new Subtraction(
+        new Value(25),
+        new Sum(
+            new Value(10), 
+            new Value(5)
+        )
+    )
+);
+
+/*
+    Will apply this formula: ((100 / (5 + 5)) * ((30 - 10) + (40 / 2))) - (50 * ((6 + 4) / 2))
+*/
+const example5 = new Subtraction(
+    new Multiplication(
+        new Divide(
+            new Value(100), 
+            new Sum(
+                new Value(5), 
+                new Value(5)
+            )
+        ),
+        new Sum(
+            new Subtraction(
+                new Value(30), 
+                new Value(10)
+            ),
+            new Divide(
+                new Value(40), 
+                new Value(2)
+            )
+        )
+    ),
+    new Multiplication(
+        new Value(50),
+        new Divide(
+            new Sum(
+                new Value(6), 
+                new Value(4)
+            ),
+            new Value(2)
+        )
+    )
+);
+
+
+console.log([example1, example2, example3, example4, example5]);
+console.log([example1.value, example2.value, example3.value, example4.value, example5.value]);
